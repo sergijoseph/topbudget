4abd1e9845c398aff1b30889258b72c0590eaf9f
Sergi Joseph <jabril.joseph@gmail.com>
2025-10-27 16:58:41 -0400

    Added privacy & terms notice signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Form, useActionData, useNavigate, useNavigation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { setAuth } from '../store/authStore'
@@ -23,6 +23,7 @@ export default function Signup() {
     const result = useActionData();
     const dispatch = useDispatch()
     const isSubmitting = navigation.state === 'submitting';
+    const [showNotice, setShowNotice] = useState(false)
 
     useEffect(() => {
 
@@ -34,13 +35,17 @@ export default function Signup() {
 
     }, [result])
 
+    function handleShowNotice() {
+        setShowNotice(prev => !prev)
+    }
+
     return (
         <ContentBox boxTitle="Sign Up" style="signup-box">
             <Form method='POST' className='signup-form'>
                 <div className="input-wrapper">
                     <img className='icons' src={iconName} />
                     <input type="text" size="8" id='firstName' name='firstName' placeholder="First Name" required />
-                    <input style={{marginLeft: '13px'}} type="text" size="7" id='lastName' name='lastName' placeholder="Last Name" required />
+                    <input style={{ marginLeft: '13px' }} type="text" size="7" id='lastName' name='lastName' placeholder="Last Name" required />
                 </div>
                 <br></br>
                 <div className="input-wrapper">
@@ -57,13 +62,50 @@ export default function Signup() {
                     <img className='icons' src={iconPassword} />
                     <input type="password" size="27" id='passwordConfirm' name='passwordConfirm' placeholder="Confirm Password" required />
                 </div>
-                <br></br><br></br>
-
+                <br />
+                <br />
                 <ButtonText disabled={isSubmitting} addStyle="button-right">{isSubmitting ? 'Signing Up' : 'Sign Up'}</ButtonText>
+
+                <span style={{ fontSize: '0.9rem' }}>By signing up you agree to the <br />
+                    <span className='show-notice' onClick={handleShowNotice}>Demo Privary & Terms Notice</span>
+                </span>
 
             </Form>
             <br />
             {result?.error && <p className='error-message'>{result.error}</p>}
+            {showNotice &&
+                <div className='signup-notice'>
+                    <p><b>Top Budget - Demo Privary & Terms Notice</b></p>
+                    <p><b>Demo Status:</b><br />
+                        Top Budget is currently provided as a demo application.<br />
+                        It is primarily intended to showcase features and allow<br />
+                        users to test budgeting functionality. Users should avoid<br />
+                        entering sensitive or real financial information.
+                    </p>
+
+                    <p><b>Data Collection:</b><br />
+                        When signing up, Top Budget collects your first name,<br />
+                        last name, email address, and budgeting transaction data.<br />
+                        This information is used solely to provide the app's<br/> functionality.
+                    </p>
+
+                    <p><b>Privacy & Security:</b><br />
+                        <li>We make reasonable efforts to protect your data.</li>
+                        <li>As a demo, your data may not be permanently stored <br />
+                            and security cannot be guaranteed.</li>
+                        <li>Your data will not be shared with third parties except<br />
+                            as needed to operate the app (e.g., sending email<br/> notifications).</li>
+                    </p>
+
+                    <p><b>Terms of use:</b><br/>
+                    By using Top Budget, you acknowledge that:<br/>
+                        <li>This is a demo application. Features may change or <br/>
+                        stop without notice.</li>
+                        <li>We are not responsible for any financial outcomes resulting<br/>
+                        from using this app.</li>
+                        <li>You will not misuse the app (e.g., spam accounts, attempt<br/> unauthorized access)</li>
+                    </p>
+                </div>}
         </ContentBox>
 
     )
